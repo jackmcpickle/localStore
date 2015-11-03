8d28467fa9fe36ad3a61c81ac0e9d79cc4351c83
Jack McNicol <jackmcpickle@gmail.com>
2015-11-03 11:17:04 +1030

    added unset
    added warning to setting null
    get returns null if a string of undefined was set

--- localStore.js
+++ localStore.js
@@ -1,16 +1,23 @@
 var localStore = {
   
   set: function(data, value) {
-    localStorage[data] = JSON.stringify(value);
+    if (value == null) {
+      return console.warn("You can not store `" + data + "` as " + value + " - use localStore.unset(). Called from " + (arguments.callee.caller.toString()));
+    }
+    return localStorage[data] = JSON.stringify(value);
   },
 
   get: function(data) {
     var rawData;
     rawData = localStorage[data];
-    if (rawData == null) {
+    if ((rawData == null) || rawData === 'undefined') {
       return null;
     }
     return JSON.parse(rawData);
+  },
+
+  unset: function(data) {
+    return localStorage[data] = null;
   },
 
   exists: function(data) {
